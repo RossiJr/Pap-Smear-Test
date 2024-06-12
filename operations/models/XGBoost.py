1b4a075c2ae6d232944f8c10a3cf4e6456cedb2b
thayrisgabriwla <83711847+thayrisgabriela@users.noreply.github.com>
2024-06-12 17:59:17 -0300

    add comments

--- operations/models/XGBoost.py
+++ operations/models/XGBoost.py
@@ -7,13 +7,14 @@ import cv2
 from operations import image_manipulation as im
 import os
 
+# Diretório onde está o dataset recortado
 CROPPED_DATASET_DIR = '../../static/cropped_dataset'
 
-
+# Função para decodificar um label
 def decode_label(label: int, label_encoder: LabelEncoder):
     return label_encoder.inverse_transform(label)
 
-
+# Função para obter um modelo salvo
 def get_model(mode: str, path: str, label_encoder_path=None):
     if mode == 'binary':
         b_model = xgb.XGBClassifier()
@@ -26,26 +27,25 @@ def get_model(mode: str, path: str, label_encoder_path=None):
         label_encode_multi.classes_ = np.load(label_encoder_path)
         return m_model, label_encode_multi
 
-
+# Função para treinar um modelo
 def train_model(mode: str, dataset: np.array, labels: np.array):
     if mode == 'binary':
         negative_value = 'Negative for intraepithelial lesion'
 
-        # Encode the labels as binary considering 0 for negative_values and 1 for the rest
+        # Codifica os labels como binários (0 para valores negativos e 1 para o resto)
         binarized_labels = np.where(labels == negative_value, 0, 1)
 
-        # Split the dataset into training and testing
-        X_train, X_test, y_train, y_test = train_test_split(dataset, binarized_labels, test_size=0.2, random_state=42,
-                                                            stratify=binarized_labels)
+        # Divide o dataset em treino e teste
+        X_train, X_test, y_train, y_test = train_test_split(dataset, binarized_labels, test_size=0.2, random_state=42, stratify=binarized_labels)
 
-        # Create the model
+        # Cria o modelo
         model = xgb.XGBClassifier(use_label_encoder=False, eval_metric='mlogloss')
 
-        # Train the model
+        # Treina o modelo
         model.fit(X_train, y_train)
         y_pred = model.predict(X_test)
 
-        # Evaluation metrics
+        # Métricas de avaliação
         print('Accuracy: ', accuracy_score(y_test, y_pred))
         print('Confusion Matrix: ', confusion_matrix(y_test, y_pred))
         print('Classification Report: ', classification_report(y_test, y_pred))
@@ -54,14 +54,14 @@ def train_model(mode: str, dataset: np.array, labels: np.array):
     elif mode == 'multiclass':
         X_train, X_test, y_train, y_test = [], [], [], []
 
-        # Encode the labels as integers
+        # Codifica os labels como inteiros
         label_encoder = LabelEncoder()
         encoded_labels = label_encoder.fit_transform(labels)
 
-        # Save this label encoder for future use
+        # Salva o label encoder para uso futuro
         np.save('../../static/label_encoder.npy', label_encoder.classes_)
 
-        # Split the dataset into training and testing
+        # Divide o dataset em treino e teste
         current_pos = 0
         for folder in os.listdir(CROPPED_DATASET_DIR):
             qtd_imgs = len(os.listdir(os.path.join(CROPPED_DATASET_DIR, folder)))
@@ -78,50 +78,52 @@ def train_model(mode: str, dataset: np.array, labels: np.array):
         model.fit(X_train, y_train)
         y_pred = model.predict(X_test)
 
-        # Evaluation metrics
+        # Métricas de avaliação
         print('Accuracy: ', accuracy_score(y_test, y_pred))
         print('Confusion Matrix: ', confusion_matrix(y_test, y_pred))
         print('Classification Report: ', classification_report(y_test, y_pred))
 
         return model
 
-
+# Função para fazer predições
 def predict(model: xgb.XGBClassifier, image: np.array):
     haralick = [im.haralick_gray_scale(image, distances=[1, 2, 4, 8], angles=[0, np.pi / 4, np.pi / 2, 3 * np.pi / 4])]
-
     return model.predict(haralick)
 
-
+# Execução principal
 if __name__ == '__main__':
+        # Carrega o dataset e labels
     # dataset = np.load('../../static/hara_dataset.npy')
     # labels = np.load('../../static/labels.npy')
 
+    # Treina o modelo multiclasse
     # multiclass_model = train_model('multiclass', dataset, labels)
-
-    # Load the model
+    
+    # Carrega o modelo multiclasse salvo
     multiclass_model = xgb.XGBClassifier()
     multiclass_model.load_model('../../static/multiclassModel.json')
 
-    # Load the label encoder for future use
+    # Carrega o label encoder salvo para uso futuro
     label_encoder_ = LabelEncoder()
     label_encoder_.classes_ = np.load('../../static/label_encoder.npy')
 
-    # Save the model
+    # Salva o modelo multiclasse
     multiclass_model.save_model('../../static/multiclassModel.json')
 
-    # Predict
+    # Faz uma predição com o modelo multiclasse em uma imagem especificada
     prediction = predict(multiclass_model, cv2.imread('../../static/530.png'))
     print(label_encoder_.inverse_transform(prediction))
 
+    # Treina o modelo binário
     # binary_model = train_model('binary', dataset, labels)
-    # #
-    # # Save the model
+    
+    # Salva o modelo
     # binary_model.save_model('../../static/binaryModel.json')
-    #
-    # # Load the model
-    # # binary_model = xgb.XGBClassifier()
-    # # binary_model.load_model('../../static/binaryModel.json')
-    #
-    # # Predict
+    
+    # Carrega o modelo
+    # binary_model = xgb.XGBClassifier()
+    # binary_model.load_model('../../static/binaryModel.json')
+    
+    # Faz predição
     # print(predict(binary_model, cv2.imread('../../static/527.png')))
     # print(predict(binary_model, cv2.imread('../../static/530.png')))
--- views.py
+++ views.py
@@ -12,59 +12,58 @@ from operations.models import EfficientNetMulticlass as enetmulti, EfficientNetB
 from Test import settings
 import operations.image_manipulation as im
 
-
+# Função para renderizar a página inicial
 def myview(request):
     return render(request, 'index.html')
 
-
+# Função para renderizar a página "about us"
 def aboutus(request):
     return render(request, 'aboutus.html')
 
-
+# Função para fazer upload de imagem
 def upload_image(request):
     if request.method == 'POST' and request.FILES['image']:
-        # Get the uploaded file
+        # Obtém o arquivo carregado
         image_file = request.FILES['image']
-        # Define the path where you want to save the uploaded image
-        # Adjust the path according to your project structure
+        # Define o caminho onde a imagem será salva (ajuste conforme necessário)
         upload_dir = os.path.join(str(settings.BASE_DIR), "static", "images", image_file.name)
         print("upload_dir: ", upload_dir)
-        # Write the uploaded file to the specified location
+        # Escreve o arquivo carregado na localização especificada
         with open(upload_dir, 'wb+') as destination:
             for chunk in image_file.chunks():
                 destination.write(chunk)
-        # Redirect to a success page or perform any other action
-        return HttpResponseRedirect(f'/?img={image_file.name}')  # Redirect to a success page
+        # Redireciona para uma página de sucesso
+        return HttpResponseRedirect(f'/?img={image_file.name}')  # Redireciona para uma página de sucesso
     else:
-        # Handle the case when no file is uploaded
-        return HttpResponseRedirect('/error/')  # Redirect to an error page
+        # Lida com o caso em que nenhum arquivo foi carregado
+        return HttpResponseRedirect('/error/')  # Redireciona para uma página de erro
 
-
+# Função para mudar a imagem exibida
 def change_image(request):
     image_url = request.GET.get('img')
     return render(request, 'index.html', {'image_url': image_url})
 
-
+# Função para converter uma imagem para escala de cinza
 def convert_to_grayscale(request):
     data_dict = json.loads(request.body.decode("utf-8"))
     if request.method == 'POST' and data_dict['image_url']:
         image_url = data_dict['image_url']
         img = Image.open(os.path.join(str(settings.BASE_DIR), "static", "images", image_url))
 
-        # Convert the image to grayscale
+        # Converte a imagem para escala de cinza
         grayscale_img = im.convert_image_to_gray_scale(img)
 
-        # Define the path where you want to save the grayscale image
+        # Define o caminho onde a imagem em escala de cinza será salva
         grayscale_img_path = os.path.join(str(settings.BASE_DIR), "static", "images", "current_altered.png")
         print("grayscale_img_path: ", grayscale_img_path)
-        # Update this with your desired path
+        # Salva a imagem em escala de cinza
         grayscale_img.save(grayscale_img_path)
-        # Return a JSON response with the path to the grayscale image
+        # Retorna uma resposta JSON com o caminho da imagem em escala de cinza
         return JsonResponse({'grayscale_image_path': '/static/images/current_altered.png'})
     else:
         return HttpResponse(status=400)
 
-
+# Função para gerar o histograma de uma imagem
 def generate_histogram(request):
     data_dict = json.loads(request.body.decode("utf-8"))
     if request.method == 'POST' and data_dict['image_url']:
@@ -72,87 +71,75 @@ def generate_histogram(request):
         img = Image.open(os.path.join(str(settings.BASE_DIR), "static", "images", image_url))
 
         if img.mode == "L":
-            # Generate the histogram for the image
+            # Gera o histograma para a imagem em escala de cinza
             histogram = im.generate_image_histogram(img)
 
-            # Return the histogram as a JSON response
+            # Retorna o histograma como uma resposta JSON
             return JsonResponse({'imgType': 'grayscale', 'histogram': histogram})
         else:
+            # Gera o histograma para cada canal da imagem HSV
             h, s, v = im.generate_image_histogram(img)
             return JsonResponse({'imgType': 'hsv', 'histogram_h': h, 'histogram_s': s, 'histogram_v': v})
     else:
         return HttpResponse(status=400)
 
-
+# Função para gerar as características de Haralick de uma imagem
 def generate_haralick_features(request):
     data_dict = json.loads(request.body.decode("utf-8"))
     if request.method == 'POST' and data_dict['image_url']:
         image_url = data_dict['image_url']
         img = cv2.imread(os.path.join(str(settings.BASE_DIR), "static", "images", image_url))
 
-        features = im.haralick_gray_scale(img, distances=[1, 2, 4, 8], angles=[0, np.pi / 4, np.pi / 2, 3 * np.pi / 4],
-                                          api_call=True)
+        # Calcula as características de Haralick
+        features = im.haralick_gray_scale(img, distances=[1, 2, 4, 8], angles=[0, np.pi / 4, np.pi / 2, 3 * np.pi / 4], api_call=True)
 
         return JsonResponse(features, safe=False)
     else:
         return HttpResponse(status=400)
 
-
+# Função para calcular os momentos de Hu de uma imagem
 def hu_moments(request):
     data_dict = json.loads(request.body.decode("utf-8"))
     if request.method == 'POST' and data_dict['image_url']:
         image_url = data_dict['image_url']
         img_type = data_dict['type']
-        # Calculate Hu Moments for the grayscale image
 
+        # Calcula os momentos de Hu para a imagem em escala de cinza
         if img_type == 'gray':
-            binary_img, hu_moments_values = im.calculate_hu_moments(
-                os.path.join(str(settings.BASE_DIR), "static", "images", image_url), img_type)
+            binary_img, hu_moments_values = im.calculate_hu_moments(os.path.join(str(settings.BASE_DIR), "static", "images", image_url), img_type)
             binary_img_path = os.path.join(str(settings.BASE_DIR), "static", "images", "current_altered.png")
 
+            # Salva a imagem binária
             cv2.imwrite(binary_img_path, binary_img)
 
-            return JsonResponse({'binary_image_path': '/static/images/current_altered.png',
-                                 'hu_moments': hu_moments_values.tolist()})
+            return JsonResponse({'binary_image_path': '/static/images/current_altered.png', 'hu_moments': hu_moments_values.tolist()})
         elif img_type == 'color':
-            hu_moments_b, hu_moments_g, hu_moments_r = im.calculate_hu_moments(
-                os.path.join(str(settings.BASE_DIR), "static", "images", image_url), img_type)
+            hu_moments_b, hu_moments_g, hu_moments_r = im.calculate_hu_moments(os.path.join(str(settings.BASE_DIR), "static", "images", image_url), img_type)
 
-            return JsonResponse({'hu_moments_b': hu_moments_b.tolist(), 'hu_moments_g': hu_moments_g.tolist(),
-                                 'hu_moments_r': hu_moments_r.tolist()})
+            return JsonResponse({'hu_moments_b': hu_moments_b.tolist(), 'hu_moments_g': hu_moments_g.tolist(), 'hu_moments_r': hu_moments_r.tolist()})
     else:
         return HttpResponse(status=400)
 
-
+# Função para classificar uma imagem
 def classify_image(request):
     data_dict = json.loads(request.body.decode("utf-8"))
     if request.method == 'POST' and data_dict['image_url'] and data_dict['model']:
         image_url = data_dict['image_url']
         img = cv2.imread(os.path.join(str(settings.BASE_DIR), "static", "images", image_url))
 
-        # Classify the image using the pre-trained XGBoost models
+        # Classifica a imagem usando modelos pré-treinados de XGBoost ou EfficientNet
         clazz = None
         clazz_proba = None
 
         if data_dict['model'] == 'xgboostBinary':
-            clazz = xgb.predict(
-                xgb.get_model('binary', os.path.join(str(settings.BASE_DIR), "static", "binaryModel.json")), img)
+            clazz = xgb.predict(xgb.get_model('binary', os.path.join(str(settings.BASE_DIR), "static", "binaryModel.json")), img)
         elif data_dict['model'] == 'xgboostMulticlass':
-            model, label_encoder = xgb.get_model('multiclass',
-                                                 os.path.join(str(settings.BASE_DIR), "static", "multiclassModel.json"),
-                                                 label_encoder_path=os.path.join(str(settings.BASE_DIR), "static",
-                                                                                 "label_encoder.npy"))
+            model, label_encoder = xgb.get_model('multiclass', os.path.join(str(settings.BASE_DIR), "static", "multiclassModel.json"), label_encoder_path=os.path.join(str(settings.BASE_DIR), "static", "label_encoder.npy"))
             clazz = label_encoder.inverse_transform(xgb.predict(model, img))
         elif data_dict['model'] == 'efficientNetMulticlass':
-            return JsonResponse(
-                {'img_class': enetmulti.classify_image(
-                    os.path.join(str(settings.BASE_DIR), "static", "images", image_url),
-                    os.path.join(str(settings.BASE_DIR), "static", "multiclassEfficientNet.pth"))})
+            return JsonResponse({'img_class': enetmulti.classify_image(os.path.join(str(settings.BASE_DIR), "static", "images", image_url), os.path.join(str(settings.BASE_DIR), "static", "multiclassEfficientNet.pth"))})
         elif data_dict['model'] == 'efficientNetBinary':
-            return JsonResponse(
-                {'img_class': enetbinary.classify_image(
-                    os.path.join(str(settings.BASE_DIR), "static", "images", image_url),
-                    os.path.join(str(settings.BASE_DIR), "static", "binaryEfficientNet.pth"))})
+            return JsonResponse({'img_class': enetbinary.classify_image(os.path.join(str(settings.BASE_DIR), "static", "images", image_url), os.path.join(str(settings.BASE_DIR), "static", "binaryEfficientNet.pth"))})
 
         return JsonResponse({'img_class': str(clazz.tolist()[0])})
     else:
